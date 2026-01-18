858f45306467ccd684a39ed0e5f85660a2f6c616
truher <joel@truher.org>
2026-01-18 15:16:16 -0800

    scenarios

--- studies/sim2026/src/main/java/org/team100/sim2026/Alliance.java
+++ studies/sim2026/src/main/java/org/team100/sim2026/Alliance.java
@@ -1,6 +1,9 @@
 package org.team100.sim2026;
 
+import org.team100.sim2026.robots.Defender;
+import org.team100.sim2026.robots.ExampleRobot;
 import org.team100.sim2026.robots.Robot;
+import org.team100.sim2026.robots.Scorer;
 
 public class Alliance {
     final Robot robot1, robot2, robot3;
@@ -9,5 +12,19 @@ public class Alliance {
         robot1 = r1;
         robot2 = r2;
         robot3 = r3;
+    }
+
+    public static Alliance balanced(AllianceColor color, SimRun sim) {
+        return new Alliance(
+                new Scorer(color, "1", 8, sim),
+                new ExampleRobot(color, "2", 8, sim),
+                new Defender(color, "3", 8, sim));
+    }
+
+    public static Alliance uniform(AllianceColor color, SimRun sim) {
+        return new Alliance(
+                new ExampleRobot(color, "1", 8, sim),
+                new ExampleRobot(color, "2", 8, sim),
+                new ExampleRobot(color, "3", 8, sim));
     }
 }
--- studies/sim2026/src/main/java/org/team100/sim2026/Scenario.java
+++ studies/sim2026/src/main/java/org/team100/sim2026/Scenario.java
@@ -1,32 +1,12 @@
 package org.team100.sim2026;
 
-import org.team100.sim2026.robots.Defender;
-import org.team100.sim2026.robots.ExampleRobot;
-import org.team100.sim2026.robots.Scorer;
-
 /** Factory for alliances for a single simulation run. */
 public class Scenario {
     public Alliance red(SimRun sim) {
-        // multi-role
-        return new Alliance(
-                new Scorer(AllianceColor.RED, "r1", 8, sim),
-                new ExampleRobot(AllianceColor.RED, "r2", 8, sim),
-                new Defender(AllianceColor.RED, "r3", 8, sim));
+        return Alliance.balanced(AllianceColor.RED, sim);
     }
 
     public Alliance blue(SimRun sim) {
-        // all-around
-        return new Alliance(
-                new ExampleRobot(AllianceColor.BLUE, "b1", 8, sim),
-                new ExampleRobot(AllianceColor.BLUE, "b2", 8, sim),
-                new ExampleRobot(AllianceColor.BLUE, "b3", 8, sim));
-    }
-
-    Alliance blueB(SimRun sim) {
-        // multi-role
-        return new Alliance(
-                new Scorer(AllianceColor.BLUE, "b1", 8, sim),
-                new ExampleRobot(AllianceColor.BLUE, "b2", 8, sim),
-                new Defender(AllianceColor.BLUE, "b3", 8, sim));
+        return Alliance.uniform(AllianceColor.BLUE, sim);
     }
 }
