0779ce6878450e94c64be04b568c824406babeed
Coolearthsky <113796041+Coolearthsky@users.noreply.github.com>
2026-01-24 15:33:28 -0800

    Shooter

---
+++ studies/ShooterTest/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,93 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import java.lang.System.Logger.Level;
+
+import org.team100.lib.config.Feedforward100;
+import org.team100.lib.config.PIDConstants;
+import org.team100.lib.logging.LoggerFactory;
+import org.team100.lib.logging.Logging;
+import org.team100.lib.logging.primitive.PrimitiveLogger;
+import org.team100.lib.motor.MotorPhase;
+import org.team100.lib.motor.NeutralMode;
+import org.team100.lib.motor.ctre.Kraken6Motor;
+import org.team100.lib.motor.rev.NeoVortexCANSparkMotor;
+import org.team100.lib.util.CanId;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private final RobotContainer m_robotContainer;
+  private final NeoVortexCANSparkMotor top; 
+  private final NeoVortexCANSparkMotor bottom; 
+
+    private static final LoggerFactory rootLogger = Logging.instance().rootLogger;
+
+  public Robot() {
+    m_robotContainer = new RobotContainer();
+    top = new NeoVortexCANSparkMotor(rootLogger,new CanId(1), NeutralMode.BRAKE, MotorPhase.FORWARD, 1, Feedforward100.makeNeoVortex(rootLogger), PIDConstants.zero(rootLogger));
+    bottom = new NeoVortexCANSparkMotor(rootLogger, new CanId(2), NeutralMode.BRAKE, MotorPhase.FORWARD, 1, Feedforward100.makeNeoVortex(rootLogger), PIDConstants.makeVelocityPID(rootLogger,1));
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+     top.setDutyCycle(1);
+     bottom.setDutyCycle(1);
+  }
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+}
